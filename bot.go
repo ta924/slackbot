f3ff59874e9c813e3ccd0d4832112110ebc89ada
Tanner Altares <Tanner.Altares@walmart.com>
2019-09-30 13:47:52 -0500

    testing

--- bot.go
+++ bot.go
@@ -116,6 +116,10 @@ func exampleEasy() []byte{
 		)
 
 	response.Channel = "GFH5Y76LU"
+	response.Type = "message"
+	response.Text = "This content can't be displayed."
+	response.User = "W45V5UHLY"
+	response.Team = "T024GHP2K"
 	//b, err := json.MarshalIndent(msg, "", "     ")
 	b, err := json.Marshal(response)
 	if err != nil {
