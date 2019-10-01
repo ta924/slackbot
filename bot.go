e0f97c63d475b20636323daee24b7f9d14985bd6
Tanner Altares <Tanner.Altares@walmart.com>
2019-10-01 09:12:16 -0500

    update slack token

--- bot.go
+++ bot.go
@@ -14,7 +14,7 @@ import (
 )
 
 // You more than likely want your "Bot User OAuth Access Token" which starts with "xoxb-"
-var api = slack.New("xoxb-2152601087-518569019028-tlO5S77IaS8dPUcJBbRgsofc",
+var api = slack.New("xoxb-2152601087-518569019028-7wKLei78Fwj6PXrk8yaHOHTu",
 	slack.OptionDebug(true),
 	slack.OptionLog(
 		log.New(os.Stdout, "slack-bot: ",
